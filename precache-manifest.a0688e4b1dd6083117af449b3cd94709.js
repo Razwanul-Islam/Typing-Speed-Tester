d55d3db1c656206f160ed6bde3a3a4254ecd34af
Razwanul Islam <razwanrakib0@gmail.com>
2020-11-11 11:33:25 +0600

    Updates

---
+++ precache-manifest.a0688e4b1dd6083117af449b3cd94709.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb797c4a1c241590de944ef61ebce950",
+    "url": "/Typing-Speed-Tester/index.html"
+  },
+  {
+    "revision": "369d1165b698d29ca0a7",
+    "url": "/Typing-Speed-Tester/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "040bb24c08aba6887065",
+    "url": "/Typing-Speed-Tester/static/css/main.573703f9.chunk.css"
+  },
+  {
+    "revision": "369d1165b698d29ca0a7",
+    "url": "/Typing-Speed-Tester/static/js/2.0f8d68f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Typing-Speed-Tester/static/js/2.0f8d68f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040bb24c08aba6887065",
+    "url": "/Typing-Speed-Tester/static/js/main.2a4cf76e.chunk.js"
+  },
+  {
+    "revision": "84de076af3a7363257ae",
+    "url": "/Typing-Speed-Tester/static/js/runtime-main.cfbf0262.js"
+  }
+]);
